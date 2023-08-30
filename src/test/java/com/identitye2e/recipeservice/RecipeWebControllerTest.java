1251c8bf78e016f3d56474122113d561688fd18f
Rowand Mohammad <rowandnajim@gmail.com>
2023-08-30 10:44:30 +0100

    Fixing RecipeWebControllerTest issues

--- src/test/java/com/identitye2e/recipeservice/RecipeWebControllerTest.java
+++ src/test/java/com/identitye2e/recipeservice/RecipeWebControllerTest.java
@@ -2,46 +2,59 @@ package com.identitye2e.recipeservice;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RecipeWebControllerTest {
+class RecipeWebControllerTest {
 
-    @Mock
-    private FileService fileService;
-
-    @InjectMocks
-    private RecipeWebController recipeWebController;
+    private RecipeWebController controller;
 
     @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    void setUp() {
+        controller = new RecipeWebController();
+        controller.fileService = new FileService();
     }
 
     @Test
-    void testIndex() throws IOException {
-        String expectedContent = "Expected Content";
-        when(fileService.readFileContent("src/main/resources/templates/index.html")).thenReturn(expectedContent);
+    void testRenderTemplate() {
+        List<RecipeResult> recipes = new ArrayList<>();
+        RecipeResult recipe = new RecipeResult();
+        recipe.setId(1);
+        recipe.setName("Sample Recipe");
+        recipe.setDescription("A tasty sample recipe.");
+        recipe.setThumbnailUrl("https://sample.url/image.jpg");
+        recipes.add(recipe);
 
-        String result = recipeWebController.index();
+        String renderedHtml = controller.renderTemplate(recipes);
 
-        assertEquals(expectedContent, result);
+        assertTrue(renderedHtml.contains("Rowand Recipes"));
+        assertTrue(renderedHtml.contains("<a href=\"/recipeDetails?id=1\">Sample Recipe</a>"));
+        assertTrue(renderedHtml.contains("A tasty sample recipe."));
+        assertTrue(renderedHtml.contains("https://sample.url/image.jpg"));
     }
 
     @Test
-    void testIndexIOException() throws IOException {
-        when(fileService.readFileContent("src/main/resources/templates/index.html")).thenThrow(IOException.class);
+    void testRenderRecipeDetailTemplate() {
+        Root recipeDetail = new Root();
+        recipeDetail.setName("Detailed Recipe");
+        recipeDetail.setDescription("A detailed description of a tasty sample recipe.");
+        recipeDetail.setThumbnailUrl("https://sample.url/image_detailed.jpg");
 
-        String result = recipeWebController.index();
+        String renderedHtml = controller.renderRecipeDetailTemplate(recipeDetail);
 
-        assertEquals(RecipeWebController.ERROR_MSG, result);
+        assertTrue(renderedHtml.contains("Rowand Recipes"));
+        assertTrue(renderedHtml.contains("Detailed Recipe"));
+        assertTrue(renderedHtml.contains("A detailed description of a tasty sample recipe."));
+        assertTrue(renderedHtml.contains("https://sample.url/image_detailed.jpg"));
     }
 
+    @Test
+    void testIndex() {
+        String renderedHtml = controller.index();
+        assertTrue(renderedHtml.contains("<h1>Recipe Search</h1>"));
+    }
 
 }
