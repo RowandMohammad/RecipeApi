697522227fd150aa3080c5d287f5ab3482556429
Rowand Mohammad <rowandnajim@gmail.com>
2024-07-22 12:00:00 +0100

    Implemented cucumber and rest assured for testing

--- src/test/java/com/identitye2e/recipeservice/RecipeWebControllerTest.java
+++ src/test/java/com/identitye2e/recipeservice/RecipeWebControllerTest.java
@@ -31,18 +31,7 @@ public class RecipeWebControllerTest {
                 .body(containsString("Recipes"));
     }
 
-    @Test
-    public void testDisplayRecipeDetails() {
-        Integer id = 3289;  // some id for testing
-        RestAssured.given()
-                .port(8080)
-                .param("id", id)
-                .get("/recipeDetails")
-                .then()
-                .statusCode(200)
-                .body(containsString("Rowand Recipes"))
-                .body(containsString("One-Pot Chicken Fajita Pasta"));
-    }
+
 
 
 }
